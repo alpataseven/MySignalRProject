b0a7ff8b47d6ee8342777873b6fd9590b8557299
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-02-26 00:27:51 +0300

    Business Layer katmanı tamamlandı.

---
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Entities;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICategoryService : IGenericService<Category>
+    {
+    }
+}
---
+++ BusinessLayer/Concrete/SocialMediaManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Entities;
+
+namespace BusinessLayer.Concrete
+{
+    public class SocialMediaManager : ISocialMediaService
+    {
+        private readonly ISocialMediaDal _socialmediaDal;
+
+        public SocialMediaManager(ISocialMediaDal socialmediaDal)
+        {
+            _socialmediaDal = socialmediaDal;
+        }
+
+        public void TAdd(SocialMedia entity)
+        {
+            _socialmediaDal.Add(entity);
+        }
+
+        public void TDelete(SocialMedia entity)
+        {
+            _socialmediaDal.Delete(entity);
+        }
+
+        public SocialMedia TGetById(int id)
+        {
+            return _socialmediaDal.GetById(id);
+        }
+
+        public List<SocialMedia> TGetListAll()
+        {
+            return _socialmediaDal.GetListAll();
+        }
+
+        public void TUpdate(SocialMedia entity)
+        {
+            _socialmediaDal.Update(entity);
+        }
+    }
+}
