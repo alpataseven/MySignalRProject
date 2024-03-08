ac1af68e48700770237dac39b857efb0801056c9
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-03-09 02:29:43 +0300

    Api Conroller Eklenmeye Başlandı.

---
+++ MyProjectAPI/Controllers/AboutController.cs
@@ -0,0 +1,64 @@
+﻿using BusinessLayer.Abstract;
+using DtoLayer.AboutDto;
+using EntityLayer.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IAboutService _aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+        [HttpGet]
+        public IActionResult AboutList()
+        {
+            var values = _aboutService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateAbout(CreateAboutDto createAboutDto)
+        {
+            About about = new About() 
+            {
+                AboutTitle = createAboutDto.AboutTitle,
+                AboutDescription = createAboutDto.AboutDescription,
+                AboutImageUrl = createAboutDto.AboutImageUrl
+            };
+            _aboutService.TAdd(about);
+            return Ok("Hakkımda Kısmı Başarılı Şekilde Eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteAbout(int id)
+        {
+            var value = _aboutService.TGetById(id);
+            _aboutService.TDelete(value);
+            return Ok("Hakkımda kısmı silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            About about = new About()
+            {
+                AboutId = updateAboutDto.AboutId,
+                AboutImageUrl = updateAboutDto.AboutImageUrl,
+                AboutTitle = updateAboutDto.AboutTitle,
+                AboutDescription = updateAboutDto.AboutDescription
+            };
+            _aboutService.TUpdate(about);
+            return Ok("Hakkımda Alanı Güncellendi");
+        }
+        [HttpGet("GetAbout")]
+        public IActionResult GetAbout(int id)
+        {
+            var value = _aboutService.TGetById(id);
+            return Ok(value);
+        }
+    }
+}
