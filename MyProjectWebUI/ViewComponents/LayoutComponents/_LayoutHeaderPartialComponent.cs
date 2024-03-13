e7227fe87d9ab5b64a59e52d303aca2ecc0f218c
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-03-13 03:47:50 +0300

    Admin teması eklendi ve düzenleme başlandı.

---
+++ MyProjectWebUI/ViewComponents/LayoutComponents/_LayoutHeaderPartialComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyProjectWebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoutHeaderPartialComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+    
+}
